08c037733d88159fc0e87740ba0601bc8ce7ac95
xuwusheng <xuwusheng@hotmail.com>
2016-12-23 08:23:13 +0800

    update use routes

--- app/router/index.js
+++ app/router/index.js
@@ -1,12 +1,14 @@
 /**
  * Created by xwatson on 2016/12/23.
  */
-import route from './path'
-
-export default (app) => {
-    app
-        .use(route.routes())
-        .use(route.allowedMethods({
-            throw: true
-        }))
+import path from './path'
+const routes = [path]
+export default function (app) {
+    routes.forEach((route) => {
+        app
+            .use(route.routes())
+            .use(route.allowedMethods({
+                throw: true
+            }))
+    })
 }
